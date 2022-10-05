339d97223c3a0948f075780fe5596b0f73fc86e6
KerryNguyen <115020015+KerryNguyen@users.noreply.github.com>
2022-10-05 13:36:54 -0400

    Simpson dot java
    Project 2: approximate a definite integral

---
+++ Simpson.java
@@ -0,0 +1,60 @@
+package ohio;
+import java.util.*;
+/*------------------------------------------------------------------------------------------------------
+ *  GitHub: KerryNguyen
+ *  I have no idea what I'm doing
+ *------------------------------------------------------------------------------------------------------*/
+class Function{                                                        
+
+    // Define the function to integrate
+    double f (double x) {                                              
+       return Math.cos(x);
+    }
+
+    // Simpson's method for integral calculus
+    // a = lower bound
+    // b = upper bound of integration
+    // n = number of passes (higher = less margin of error, but takes longer)
+    double IntSimpson(double a, double b,int n){                       
+       int i,z;                                                       
+       double h,s;                                                    
+
+       n=n+n;
+       s = f(a)*f(b);
+       h = (b-a)/n;                                        
+       z = 4;
+
+       for(i = 1; i<n; i++){
+          s = s + z * f(a+i*h);
+          z = 6 - z;
+       }
+       return (s * h)/3;
+    } 
+
+
+
+class integration{                                                    
+
+    // Class result: calculates the integral and displays the result.
+    public static void main(String args[]){
+       // Call class function                                           
+       Function function;                                   
+       function = new Function();
+       Scanner sc = new Scanner(System.in);
+
+       // ENTER the desired values of a, b and n !!!
+       System.out.print("Enter the value of a: ");
+       double a = sc.nextInt();              
+       System.out.print("Enter the value of b: ");
+       double b = sc.nextInt();
+       System.out.print("Enter the value of n: ");
+       int n = sc.nextInt();
+       // Applies Simpson's method to function
+       double result = function.IntSimpson(a,b,n);
+
+       // Show results
+       System.out.println("Integral is: " + result);    
+       sc.close();
+    }
+}
+}
